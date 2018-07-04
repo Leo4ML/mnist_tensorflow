7d4a9270c4a0e235dc232fc8b5623c04c5672a7e
Leo4ML <838551198@qq.com>
2018-07-04 21:46:50 +0800

    神经网络中对手写体应用dropout防止过拟合测试

---
+++ mnist_tensorflow.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 23:10:06 2018
+
+@author: Leo
+"""
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+ 
+batch_size = 100
+n_batch = mnist.train.num_examples//batch_size
+x = tf.placeholder(tf.float32, [None,784])
+y = tf.placeholder(tf.float32, [None,10])
+keep_prob = tf.placeholder(tf.float32)
+ #第一层
+w1 = tf.Variable(tf.truncated_normal([784,2000],stddev=0.1))
+b1 = tf.Variable(tf.zeros([2000])+0.1)
+L1 = tf.nn.tanh(tf.matmul(x,w1)+b1)
+#dropout方法，keep_prob表示有多少比例的神经元在工作
+L1_drop = tf.nn.dropout(L1,keep_prob)
+
+
+#第二层
+w2 = tf.Variable(tf.truncated_normal([2000,2000],stddev=0.1))
+b2 = tf.Variable(tf.zeros([2000])+0.1)
+L2 = tf.nn.softmax(tf.matmul(L1,w2)+b2)
+L2_drop = tf.nn.dropout(L2,keep_prob)
+
+
+w3 = tf.Variable(tf.truncated_normal([2000,1000],stddev=0.1))
+b3 = tf.Variable(tf.zeros([1000])+0.1)
+L3 = tf.nn.softmax(tf.matmul(L1,w2)+b3)
+L3_drop = tf.nn.dropout(L3,keep_prob)
+
+
+w4 = tf.Variable(tf.truncated_normal([1000,10],stddev=0.1))
+b4 = tf.Variable(tf.zeros([10])+0.1)
+L4 = tf.nn.softmax(tf.matmul(L1,w2)+b4)
+L4_drop = tf.nn.dropout(L4,keep_prob)
+
+#loss = tf.reduce_mean(tf.square(y-L2))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=L2))
+train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+
+init = tf.global_variables_initializer()
+
+correct_prediction = tf.equal(tf.argmax(L2,1), tf.argmax(y,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(101):
+        for o in range(n_batch):
+            batch_xs,batch_ys = mnist.train.next_batch(batch_size)
+            sess.run(train_step,feed_dict={y:batch_ys,x:batch_xs})
+        acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels})
+        if i%10==0:
+            print('第'+str(i)+'次准确率为：'+str(acc))
+    
