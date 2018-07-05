7803fa376386068454eb495d2d5d3ac400b42c67
Leo4ML <838551198@qq.com>
2018-07-05 14:38:13 +0800

    纠正准确率中的L2变量为predicton，这也是造成准确率一直难以变化的原因

--- mnist_tensorflow.py
+++ mnist_tensorflow.py
@@ -26,38 +26,39 @@ L1_drop = tf.nn.dropout(L1,keep_prob)
 #第二层
 w2 = tf.Variable(tf.truncated_normal([2000,2000],stddev=0.1))
 b2 = tf.Variable(tf.zeros([2000])+0.1)
-L2 = tf.nn.softmax(tf.matmul(L1,w2)+b2)
+L2 = tf.nn.tanh(tf.matmul(L1_drop,w2)+b2)
 L2_drop = tf.nn.dropout(L2,keep_prob)
 
 
 w3 = tf.Variable(tf.truncated_normal([2000,1000],stddev=0.1))
 b3 = tf.Variable(tf.zeros([1000])+0.1)
-L3 = tf.nn.softmax(tf.matmul(L1,w2)+b3)
+L3 = tf.nn.tanh(tf.matmul(L2_drop,w3)+b3)
 L3_drop = tf.nn.dropout(L3,keep_prob)
 
 
 w4 = tf.Variable(tf.truncated_normal([1000,10],stddev=0.1))
 b4 = tf.Variable(tf.zeros([10])+0.1)
-L4 = tf.nn.softmax(tf.matmul(L1,w2)+b4)
-L4_drop = tf.nn.dropout(L4,keep_prob)
+prediction = tf.nn.softmax(tf.matmul(L3_drop,w4)+b4)
 
-#loss = tf.reduce_mean(tf.square(y-L2))
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=L2))
+
+#loss = tf.reduce_mean(tf.square(y-L4))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=prediction))
 train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
 
 init = tf.global_variables_initializer()
 
-correct_prediction = tf.equal(tf.argmax(L2,1), tf.argmax(y,1))
+correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
 with tf.Session() as sess:
     sess.run(init)
-    for i in range(101):
+    for i in range(31):
         for o in range(n_batch):
             batch_xs,batch_ys = mnist.train.next_batch(batch_size)
-            sess.run(train_step,feed_dict={y:batch_ys,x:batch_xs})
-        acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels})
-        if i%10==0:
-            print('第'+str(i)+'次准确率为：'+str(acc))
+            sess.run(train_step,feed_dict={y:batch_ys,x:batch_xs,keep_prob:1.0})
+        test_acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0})
+        train_acc = sess.run(accuracy,feed_dict={x:mnist.train.images,y:mnist.train.labels,keep_prob:1.0})
+        if i%3==0:
+            print('第'+str(i)+'次训练准确率为：'+str(train_acc)+'测试准确率为：'+str(test_acc))
     
