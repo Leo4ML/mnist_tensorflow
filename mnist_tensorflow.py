f4527cfd84017692805b72d70e1f5dde9bd53a8c
Leo4ML <838551198@qq.com>
2018-07-05 17:19:28 +0800

    增加不同优化器的数据对比

--- mnist_tensorflow.py
+++ mnist_tensorflow.py
@@ -22,29 +22,29 @@ L1 = tf.nn.tanh(tf.matmul(x,w1)+b1)
 #dropout方法，keep_prob表示有多少比例的神经元在工作
 L1_drop = tf.nn.dropout(L1,keep_prob)
 
-'''
+
 #第二层
-w2 = tf.Variable(tf.truncated_normal([2000,2000],stddev=0.1))
-b2 = tf.Variable(tf.zeros([2000])+0.1)
+w2 = tf.Variable(tf.truncated_normal([200,100],stddev=0.1))
+b2 = tf.Variable(tf.zeros([100])+0.1)
 L2 = tf.nn.tanh(tf.matmul(L1_drop,w2)+b2)
 L2_drop = tf.nn.dropout(L2,keep_prob)
 
 
-w3 = tf.Variable(tf.truncated_normal([2000,1000],stddev=0.1))
-b3 = tf.Variable(tf.zeros([1000])+0.1)
+w3 = tf.Variable(tf.truncated_normal([100,50],stddev=0.1))
+b3 = tf.Variable(tf.zeros([50])+0.1)
 L3 = tf.nn.tanh(tf.matmul(L2_drop,w3)+b3)
 L3_drop = tf.nn.dropout(L3,keep_prob)
 
-'''
-w4 = tf.Variable(tf.truncated_normal([200,10],stddev=0.1))
+
+w4 = tf.Variable(tf.truncated_normal([50,10],stddev=0.1))
 b4 = tf.Variable(tf.zeros([10])+0.1)
-prediction = tf.nn.softmax(tf.matmul(L1_drop,w4)+b4)
+prediction = tf.nn.softmax(tf.matmul(L3_drop,w4)+b4)
 
 
 #loss = tf.reduce_mean(tf.square(y-L4))
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=prediction))
 #train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
-train_step = tf.train.AdamOptimizer(0.01).minimize(loss)
+train_step = tf.train.MomentumOptimizer(0.003,0.9).minimize(loss)
 
 init = tf.global_variables_initializer()
 
@@ -54,7 +54,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
 with tf.Session() as sess:
     sess.run(init)
-    for i in range(19):
+    for i in range(25):
         for o in range(n_batch):
             batch_xs,batch_ys = mnist.train.next_batch(batch_size)
             sess.run(train_step,feed_dict={y:batch_ys,x:batch_xs,keep_prob:0.7})
